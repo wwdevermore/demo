47489a68abb8d4ddc50f7879f87a9dbd8ab59565
vida.wang <476375300@qq.com>
2017-07-25 23:31:42 +0800

    Update Constant.java

--- src/main/java/Util/Constant.java
+++ src/main/java/Util/Constant.java
@@ -4,8 +4,8 @@ package Util;
  * Created by wangw on 2017/7/25.
  */
 public class Constant {
-        public static final String loginUrl="http://demo.1hai.cn/ssologin/loginservice.asmx?op=LoginV2";
-        public static final String systemCode="WXZX";
+        public static final String loginUrl="";
+        public static final String systemCode="";
         public static final String systemKey="WXZX20161222";
         public static final String DES_KEY = "ssologin";
         public static final String DES_IV = "ssologin";
